catppuccin/iterm

commit 7a71cf49c88f9d729ffebeec9ddb9e83b09a0d00

feat: use dark/light mode syncing colorscheme format (#18)

build.ts
@@ -1,147 +1,86 @@
-#!/usr/bin/env -S deno run --allow-write --allow-env
-import { variants } from "https://esm.sh/@catppuccin/palette@0.1.5";
-import Handlebars from "https://esm.sh/handlebars@4.7.7";
+#!/usr/bin/env -S deno run --allow-write
+import {
+  Colors,
+  flavorEntries,
+  FlavorName,
+  flavors,
+} from "https://deno.land/x/catppuccin@v1.1.1/mod.ts";
+import plist from "https://esm.sh/v135/plist@3.1.0";
 
-const template = `<?xml version="1.0" encoding="UTF-8"?>
-<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-<plist version="1.0">
-<dict>
-  {{#each color}}
-  <key>{{key}}</key>
-  <dict>
-    <key>Color Space</key>
-    <string>sRGB</string>
-    <key>Red Component</key>
-    <real>{{col.red}}</real>
-    <key>Green Component</key>
-    <real>{{col.green}}</real>
-    <key>Blue Component</key>
-    <real>{{col.blue}}</real>
-    <key>Alpha Component</key>
-    <real>{{col.alpha}}</real>
-  </dict>
-  {{/each}}
-</dict>
-</plist>`;
+type ITermColor = {
+  "Red Component": number;
+  "Green Component": number;
+  "Blue Component": number;
+  "Alpha Component": number;
+  "Color Space": "sRGB";
+};
 
-for (let [flavour, colors] of Object.entries(variants)) {
-  const isLatte = flavour === "latte";
+const termcolors = (flavorName: FlavorName, suffix?: "Dark" | "Light") => {
+  const { dark, ...flavor } = flavors[flavorName];
 
-  colors = Object.fromEntries(
-    Object.entries(colors).map(([key, value]) => {
-      const [red, green, blue] = [
-        value.hex.slice(1, 3),
-        value.hex.slice(3, 5),
-        value.hex.slice(5, 7),
-      ].map((v) => parseInt(v, 16) / 255);
-      return [key, { red, green, blue, alpha: 1 }];
+  const colors = flavor.colorEntries.reduce((acc, [colorName, color]) => {
+    // iTerm needs rgb colors between 0.0 and 1.0
+    const [red, green, blue] = [color.rgb.r, color.rgb.g, color.rgb.b]
+      .map((v) => v / 255);
+
+    return {
+      [colorName]: {
+        "Red Component": red,
+        "Green Component": green,
+        "Blue Component": blue,
+        "Alpha Component": 1.0,
+        "Color Space": "sRGB",
+      },
+      ...acc,
+    };
+  }, {} as Colors<ITermColor>);
+
+  const mappings = {
+    "Ansi 0 Color": dark ? colors.surface1 : colors.subtext1,
+    "Ansi 1 Color": colors.red,
+    "Ansi 2 Color": colors.green,
+    "Ansi 3 Color": colors.yellow,
+    "Ansi 4 Color": colors.blue,
+    "Ansi 5 Color": colors.pink,
+    "Ansi 6 Color": colors.teal,
+    "Ansi 7 Color": dark ? colors.subtext1 : colors.surface2,
+    "Ansi 8 Color": dark ? colors.surface2 : colors.subtext0,
+    "Ansi 9 Color": colors.red,
+    "Ansi 10 Color": colors.green,
+    "Ansi 11 Color": colors.yellow,
+    "Ansi 12 Color": colors.blue,
+    "Ansi 13 Color": colors.pink,
+    "Ansi 14 Color": colors.teal,
+    "Ansi 15 Color": dark ? colors.surface1 : colors.subtext0,
+    "Background Color": colors.base,
+    "Foreground Color": colors.text,
+    "Link Color": colors.sky,
+    "Bold Color": colors.text,
+    "Cursor Color": colors.rosewater,
+    "Cursor Text Color": colors.base,
+    "Cursor Guide Color": { ...colors.text, alpha: 0.07 },
+    "Selection Color": colors.surface2,
+    "Selected Text Color": colors.text,
+  };
+
+  return Object.entries(mappings).reduce(
+    (acc, [k, v]) => ({
+      [suffix ? `${k} (${suffix})` : k]: v,
+      ...acc,
     }),
+    {} as Record<string, ITermColor>,
   );
-  const termcolor = [
-    {
-      key: "Ansi 0 Color",
-      col: isLatte ? colors.subtext1 : colors.surface1,
-    },
-    {
-      key: "Ansi 1 Color",
-      col: colors.red,
-    },
-    {
-      key: "Ansi 2 Color",
-      col: colors.green,
-    },
-    {
-      key: "Ansi 3 Color",
-      col: colors.yellow,
-    },
-    {
-      key: "Ansi 4 Color",
-      col: colors.blue,
-    },
-    {
-      key: "Ansi 5 Color",
-      col: colors.pink,
-    },
-    {
-      key: "Ansi 6 Color",
-      col: colors.teal,
-    },
-    {
-      key: "Ansi 7 Color",
-      col: isLatte ? colors.surface2 : colors.subtext1,
-    },
-    {
-      key: "Ansi 8 Color",
-      col: isLatte ? colors.subtext0 : colors.surface2,
-    },
-    {
-      key: "Ansi 9 Color",
-      col: colors.red,
-    },
-    {
-      key: "Ansi 10 Color",
-      col: colors.green,
-    },
-    {
-      key: "Ansi 11 Color",
-      col: colors.yellow,
-    },
-    {
-      key: "Ansi 12 Color",
-      col: colors.blue,
-    },
-    {
-      key: "Ansi 13 Color",
-      col: colors.pink,
-    },
-    {
-      key: "Ansi 14 Color",
-      col: colors.teal,
-    },
-    {
-      key: "Ansi 15 Color",
-      col: isLatte ? colors.surface1 : colors.subtext0,
-    },
-    {
-      key: "Background Color",
-      col: colors.base,
-    },
-    {
-      key: "Foreground Color",
-      col: colors.text,
-    },
-    {
-      key: "Link Color",
-      col: colors.sky,
-    },
-    {
-      key: "Bold Color",
-      col: colors.text,
-    },
-    {
-      key: "Cursor Color",
-      col: colors.rosewater,
-    },
-    {
-      key: "Cursor Text Color",
-      col: colors.base,
-    },
-    {
-      key: "Cursor Guide Color",
-      col: { ...colors.text, alpha: 0.07 },
-    },
-    {
-      key: "Selection Color",
-      col: colors.surface2,
-    },
-    {
-      key: "Selected Text Color",
-      col: colors.text,
-    },
-  ];
-  const compiled = Handlebars.compile(template);
+};
+
+flavorEntries.map(([flavorName]) => {
+  const combined = {
+    ...termcolors(flavorName),
+    ...termcolors("latte", "Light"),
+    ...termcolors(flavorName, "Dark"),
+  };
+
   Deno.writeTextFileSync(
-    `./colors/catppuccin-${flavour}.itermcolors`,
-    compiled({ color: termcolor }),
+    `./colors/catppuccin-${flavorName}.itermcolors`,
+    plist.build(combined),
   );
-}
+});

commit 6b5765b3d701fb21e6c250fcd0d90bc65e50d031

fix: regressions from #18 (#22)
Fix ANSI colour 15 & cursor line contrast

We miss you Winston <3

build.ts
@@ -51,14 +51,14 @@ const termcolors = (flavorName: FlavorName, suffix?: "Dark" | "Light") => {
     "Ansi 12 Color": colors.blue,
     "Ansi 13 Color": colors.pink,
     "Ansi 14 Color": colors.teal,
-    "Ansi 15 Color": dark ? colors.surface1 : colors.subtext0,
+    "Ansi 15 Color": dark ? colors.subtext0 : colors.surface1,
     "Background Color": colors.base,
     "Foreground Color": colors.text,
     "Link Color": colors.sky,
     "Bold Color": colors.text,
     "Cursor Color": colors.rosewater,
     "Cursor Text Color": colors.base,
-    "Cursor Guide Color": { ...colors.text, alpha: 0.07 },
+    "Cursor Guide Color": { ...colors.text, "Alpha Component": 0.07 },
     "Selection Color": colors.surface2,
     "Selected Text Color": colors.text,
   };

colors/catppuccin-frappe.itermcolors
@@ -37,11 +37,9 @@
       <key>Blue Component</key>
       <real>0.9607843137254902</real>
       <key>Alpha Component</key>
-      <integer>1</integer>
+      <real>0.07</real>
       <key>Color Space</key>
       <string>sRGB</string>
-      <key>alpha</key>
-      <real>0.07</real>
     </dict>
     <key>Cursor Text Color</key>
     <dict>
@@ -124,11 +122,11 @@
     <key>Ansi 15 Color</key>
     <dict>
       <key>Red Component</key>
-      <real>0.3176470588235294</real>
+      <real>0.6470588235294118</real>
       <key>Green Component</key>
-      <real>0.3411764705882353</real>
+      <real>0.6784313725490196</real>
       <key>Blue Component</key>
-      <real>0.42745098039215684</real>
+      <real>0.807843137254902</real>
       <key>Alpha Component</key>
       <integer>1</integer>
       <key>Color Space</key>
@@ -364,11 +362,9 @@
       <key>Blue Component</key>
       <real>0.4117647058823529</real>
       <key>Alpha Component</key>
-      <integer>1</integer>
+      <real>0.07</real>
       <key>Color Space</key>
       <string>sRGB</string>
-      <key>alpha</key>
-      <real>0.07</real>
     </dict>
     <key>Cursor Text Color (Light)</key>
     <dict>
@@ -451,11 +447,11 @@
     <key>Ansi 15 Color (Light)</key>
     <dict>
       <key>Red Component</key>
-      <real>0.4235294117647059</real>
+      <real>0.7372549019607844</real>
       <key>Green Component</key>
-      <real>0.43529411764705883</real>
+      <real>0.7529411764705882</real>
       <key>Blue Component</key>
-      <real>0.5215686274509804</real>
+      <real>0.8</real>
       <key>Alpha Component</key>
       <integer>1</integer>
       <key>Color Space</key>
@@ -691,11 +687,9 @@
       <key>Blue Component</key>
       <real>0.9607843137254902</real>
       <key>Alpha Component</key>
-      <integer>1</integer>
+      <real>0.07</real>
       <key>Color Space</key>
       <string>sRGB</string>
-      <key>alpha</key>
-      <real>0.07</real>
     </dict>
     <key>Cursor Text Color (Dark)</key>
     <dict>
@@ -778,11 +772,11 @@
     <key>Ansi 15 Color (Dark)</key>
     <dict>
       <key>Red Component</key>
-      <real>0.3176470588235294</real>
+      <real>0.6470588235294118</real>
       <key>Green Component</key>
-      <real>0.3411764705882353</real>
+      <real>0.6784313725490196</real>
       <key>Blue Component</key>
-      <real>0.42745098039215684</real>
+      <real>0.807843137254902</real>
       <key>Alpha Component</key>
       <integer>1</integer>
       <key>Color Space</key>

colors/catppuccin-latte.itermcolors
@@ -37,11 +37,9 @@
       <key>Blue Component</key>
       <real>0.4117647058823529</real>
       <key>Alpha Component</key>
-      <integer>1</integer>
+      <real>0.07</real>
       <key>Color Space</key>
       <string>sRGB</string>
-      <key>alpha</key>
-      <real>0.07</real>
     </dict>
     <key>Cursor Text Color</key>
     <dict>
@@ -124,11 +122,11 @@
     <key>Ansi 15 Color</key>
     <dict>
       <key>Red Component</key>
-      <real>0.4235294117647059</real>
+      <real>0.7372549019607844</real>
       <key>Green Component</key>
-      <real>0.43529411764705883</real>
+      <real>0.7529411764705882</real>
       <key>Blue Component</key>
-      <real>0.5215686274509804</real>
+      <real>0.8</real>
       <key>Alpha Component</key>
       <integer>1</integer>
       <key>Color Space</key>
@@ -364,11 +362,9 @@
       <key>Blue Component</key>
       <real>0.4117647058823529</real>
       <key>Alpha Component</key>
-      <integer>1</integer>
+      <real>0.07</real>
       <key>Color Space</key>
       <string>sRGB</string>
-      <key>alpha</key>
-      <real>0.07</real>
     </dict>
     <key>Cursor Text Color (Light)</key>
     <dict>
@@ -451,11 +447,11 @@
     <key>Ansi 15 Color (Light)</key>
     <dict>
       <key>Red Component</key>
-      <real>0.4235294117647059</real>
+      <real>0.7372549019607844</real>
       <key>Green Component</key>
-      <real>0.43529411764705883</real>
+      <real>0.7529411764705882</real>
       <key>Blue Component</key>
-      <real>0.5215686274509804</real>
+      <real>0.8</real>
       <key>Alpha Component</key>
       <integer>1</integer>
       <key>Color Space</key>
@@ -691,11 +687,9 @@
       <key>Blue Component</key>
       <real>0.4117647058823529</real>
       <key>Alpha Component</key>
-      <integer>1</integer>
+      <real>0.07</real>
       <key>Color Space</key>
       <string>sRGB</string>
-      <key>alpha</key>
-      <real>0.07</real>
     </dict>
     <key>Cursor Text Color (Dark)</key>
     <dict>
@@ -778,11 +772,11 @@
     <key>Ansi 15 Color (Dark)</key>
     <dict>
       <key>Red Component</key>
-      <real>0.4235294117647059</real>
+      <real>0.7372549019607844</real>
       <key>Green Component</key>
-      <real>0.43529411764705883</real>
+      <real>0.7529411764705882</real>
       <key>Blue Component</key>
-      <real>0.5215686274509804</real>
+      <real>0.8</real>
       <key>Alpha Component</key>
       <integer>1</integer>
       <key>Color Space</key>

colors/catppuccin-macchiato.itermcolors
@@ -37,11 +37,9 @@
       <key>Blue Component</key>
       <real>0.9607843137254902</real>
       <key>Alpha Component</key>
-      <integer>1</integer>
+      <real>0.07</real>
       <key>Color Space</key>
       <string>sRGB</string>
-      <key>alpha</key>
-      <real>0.07</real>
     </dict>
     <key>Cursor Text Color</key>
     <dict>
@@ -124,11 +122,11 @@
     <key>Ansi 15 Color</key>
     <dict>
       <key>Red Component</key>
-      <real>0.28627450980392155</real>
+      <real>0.6470588235294118</real>
       <key>Green Component</key>
-      <real>0.30196078431372547</real>
+      <real>0.6784313725490196</real>
       <key>Blue Component</key>
-      <real>0.39215686274509803</real>
+      <real>0.796078431372549</real>
       <key>Alpha Component</key>
       <integer>1</integer>
       <key>Color Space</key>
@@ -364,11 +362,9 @@
       <key>Blue Component</key>
       <real>0.4117647058823529</real>
       <key>Alpha Component</key>
-      <integer>1</integer>
+      <real>0.07</real>
       <key>Color Space</key>
       <string>sRGB</string>
-      <key>alpha</key>
-      <real>0.07</real>
     </dict>
     <key>Cursor Text Color (Light)</key>
     <dict>
@@ -451,11 +447,11 @@
     <key>Ansi 15 Color (Light)</key>
     <dict>
       <key>Red Component</key>
-      <real>0.4235294117647059</real>
+      <real>0.7372549019607844</real>
       <key>Green Component</key>
-      <real>0.43529411764705883</real>
+      <real>0.7529411764705882</real>
       <key>Blue Component</key>
-      <real>0.5215686274509804</real>
+      <real>0.8</real>
       <key>Alpha Component</key>
       <integer>1</integer>
       <key>Color Space</key>
@@ -691,11 +687,9 @@
       <key>Blue Component</key>
       <real>0.9607843137254902</real>
       <key>Alpha Component</key>
-      <integer>1</integer>
+      <real>0.07</real>
       <key>Color Space</key>
       <string>sRGB</string>
-      <key>alpha</key>
-      <real>0.07</real>
     </dict>
     <key>Cursor Text Color (Dark)</key>
     <dict>
@@ -778,11 +772,11 @@
     <key>Ansi 15 Color (Dark)</key>
     <dict>
       <key>Red Component</key>
-      <real>0.28627450980392155</real>
+      <real>0.6470588235294118</real>
       <key>Green Component</key>
-      <real>0.30196078431372547</real>
+      <real>0.6784313725490196</real>
       <key>Blue Component</key>
-      <real>0.39215686274509803</real>
+      <real>0.796078431372549</real>
       <key>Alpha Component</key>
       <integer>1</integer>
       <key>Color Space</key>

colors/catppuccin-mocha.itermcolors
@@ -37,11 +37,9 @@
       <key>Blue Component</key>
       <real>0.9568627450980393</real>
       <key>Alpha Component</key>
-      <integer>1</integer>
+      <real>0.07</real>
       <key>Color Space</key>
       <string>sRGB</string>
-      <key>alpha</key>
-      <real>0.07</real>
     </dict>
     <key>Cursor Text Color</key>
     <dict>
@@ -124,11 +122,11 @@
     <key>Ansi 15 Color</key>
     <dict>
       <key>Red Component</key>
-      <real>0.27058823529411763</real>
+      <real>0.6509803921568628</real>
       <key>Green Component</key>
-      <real>0.2784313725490196</real>
+      <real>0.6784313725490196</real>
       <key>Blue Component</key>
-      <real>0.35294117647058826</real>
+      <real>0.7843137254901961</real>
       <key>Alpha Component</key>
       <integer>1</integer>
       <key>Color Space</key>
@@ -364,11 +362,9 @@
       <key>Blue Component</key>
       <real>0.4117647058823529</real>
       <key>Alpha Component</key>
-      <integer>1</integer>
+      <real>0.07</real>
       <key>Color Space</key>
       <string>sRGB</string>
-      <key>alpha</key>
-      <real>0.07</real>
     </dict>
     <key>Cursor Text Color (Light)</key>
     <dict>
@@ -451,11 +447,11 @@
     <key>Ansi 15 Color (Light)</key>
     <dict>
       <key>Red Component</key>
-      <real>0.4235294117647059</real>
+      <real>0.7372549019607844</real>
       <key>Green Component</key>
-      <real>0.43529411764705883</real>
+      <real>0.7529411764705882</real>
       <key>Blue Component</key>
-      <real>0.5215686274509804</real>
+      <real>0.8</real>
       <key>Alpha Component</key>
       <integer>1</integer>
       <key>Color Space</key>
@@ -691,11 +687,9 @@
       <key>Blue Component</key>
       <real>0.9568627450980393</real>
       <key>Alpha Component</key>
-      <integer>1</integer>
+      <real>0.07</real>
       <key>Color Space</key>
       <string>sRGB</string>
-      <key>alpha</key>
-      <real>0.07</real>
     </dict>
     <key>Cursor Text Color (Dark)</key>
     <dict>
@@ -778,11 +772,11 @@
     <key>Ansi 15 Color (Dark)</key>
     <dict>
       <key>Red Component</key>
-      <real>0.27058823529411763</real>
+      <real>0.6509803921568628</real>
       <key>Green Component</key>
-      <real>0.2784313725490196</real>
+      <real>0.6784313725490196</real>
       <key>Blue Component</key>
-      <real>0.35294117647058826</real>
+      <real>0.7843137254901961</real>
       <key>Alpha Component</key>
       <integer>1</integer>
       <key>Color Space</key>