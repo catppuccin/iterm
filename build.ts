6bc7beeb1c1ba403eb243efce81e27c189b6fed6
uncenter <uncenter@uncenter.dev>
2025-03-01 16:58:43 -0500

    feat: use new ansi colors (#29)

--- build.ts
+++ build.ts
@@ -1,10 +1,11 @@
 #!/usr/bin/env -S deno run --allow-write
 import {
-  Colors,
+  type ColorFormat,
+  type Colors,
   flavorEntries,
-  FlavorName,
+  type FlavorName,
   flavors,
-} from "https://deno.land/x/catppuccin@v1.1.1/mod.ts";
+} from "https://deno.land/x/catppuccin@v1.7.1/mod.ts";
 import plist from "https://esm.sh/v135/plist@3.1.0";
 
 type ITermColor = {
@@ -15,43 +16,37 @@ type ITermColor = {
   "Color Space": "sRGB";
 };
 
+function toItermColor(rgb: ColorFormat["rgb"]): ITermColor {
+  const [red, green, blue] = [rgb.r, rgb.g, rgb.b].map((v) => v / 255);
+
+  return {
+    "Red Component": red,
+    "Green Component": green,
+    "Blue Component": blue,
+    "Alpha Component": 1.0,
+    "Color Space": "sRGB",
+  };
+}
+
 const termcolors = (flavorName: FlavorName, suffix?: "Dark" | "Light") => {
-  const { dark, ...flavor } = flavors[flavorName];
+  const flavor = flavors[flavorName];
+
+  const ansiColors = Object.fromEntries(
+    flavor.ansiColorEntries
+      .flatMap(([_, { normal, bright }]) => [normal, bright])
+      .toSorted((a, b) => a.code - b.code)
+      .map(({ code, rgb }) => [`Ansi ${code} Color`, toItermColor(rgb)]),
+  );
 
   const colors = flavor.colorEntries.reduce((acc, [colorName, color]) => {
-    // iTerm needs rgb colors between 0.0 and 1.0
-    const [red, green, blue] = [color.rgb.r, color.rgb.g, color.rgb.b]
-      .map((v) => v / 255);
-
     return {
-      [colorName]: {
-        "Red Component": red,
-        "Green Component": green,
-        "Blue Component": blue,
-        "Alpha Component": 1.0,
-        "Color Space": "sRGB",
-      },
+      [colorName]: toItermColor(color.rgb),
       ...acc,
     };
   }, {} as Colors<ITermColor>);
 
   const mappings = {
-    "Ansi 0 Color": dark ? colors.surface1 : colors.subtext1,
-    "Ansi 1 Color": colors.red,
-    "Ansi 2 Color": colors.green,
-    "Ansi 3 Color": colors.yellow,
-    "Ansi 4 Color": colors.blue,
-    "Ansi 5 Color": colors.pink,
-    "Ansi 6 Color": colors.teal,
-    "Ansi 7 Color": dark ? colors.subtext1 : colors.surface2,
-    "Ansi 8 Color": dark ? colors.surface2 : colors.subtext0,
-    "Ansi 9 Color": colors.red,
-    "Ansi 10 Color": colors.green,
-    "Ansi 11 Color": colors.yellow,
-    "Ansi 12 Color": colors.blue,
-    "Ansi 13 Color": colors.pink,
-    "Ansi 14 Color": colors.teal,
-    "Ansi 15 Color": dark ? colors.subtext0 : colors.surface1,
+    ...ansiColors,
     "Background Color": colors.base,
     "Foreground Color": colors.text,
     "Link Color": colors.sky,
